2942edbba1d56b51acb01a7ba804c7028af1f6ae
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2024-02-18 18:50:27 +0300

    refactored controller script

--- controller-script/controller-script.ino
+++ controller-script/controller-script.ino
@@ -1,5 +1,4 @@
 #include <EEPROM.h>
-//#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
@@ -8,21 +7,18 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>                        // https://github.com/tzapu/WiFiManager
 #include <ESP8266SSDP.h>
-#include <Ticker.h>
+#include <ESP8266WiFi.h>
 
 #define ONE_WIRE_BUS D4                       // Шина данных подключена к выводу D4 на ESP8266. Пин подключения OneWire шины, 2 (D4)
 
 ESP8266WebServer HTTP(80);                      // Web интерфейс для устройства
-WiFiServer SERVERaREST(8080);                   // aREST и сервер для него
 
 OneWire oneWire(ONE_WIRE_BUS);                  // Настройка объекта oneWire для связи с любыми устройствами OneWire
 DallasTemperature sensors(&oneWire);            // Передаем ссылку на объект oneWire объекту Dallas Temperature.
 DeviceAddress Thermometer;                      // Переменная для хранения адресов устройств
 
-Ticker tempTicker;
-Ticker tempLimitTicker;
-Ticker delayTicker;
-
+uint32_t prevTempLimitMillis = 0;
+uint32_t prevSendTempMillis = 0;
 int httpCounter = 0;
 float operatingMaximum = 0;
 float operatingMinimum = 0;
@@ -31,6 +27,9 @@ float operatingMinimumEeprom = 0;
 String temperature, params;
 const char *host = "api.apinjener.ru";          // Адрес нашего веб сервера
 const int httpPort = 80;                        // Адрес порта для HTTPS= 443 или HTTP = 80
+
+const int FETCH_TEMP_LIMITS_INTERVAL = 15000;
+const int SEND_TEMP_INTERVAL = 10000;
 //------------------------------------------------------------------------
 int sensorId = 2;
 const char *nameAP = "Drying Chamber_2"; 
@@ -41,28 +40,34 @@ void setup()
 {
   Serial.begin(115200);
   EEPROM.begin(200);   // для esp8266/esp32
+  delay(500);
   //wifiManager.resetSettings();
   pinMode(D6, OUTPUT);
   wifiManager.setConfigPortalTimeout(120); // auto close configportal after n seconds
   wifiManager.autoConnect(nameAP);    //Если не удалось подключиться клиентом запускаем режим AP. Доступ к настройкам по адресу http://192.168.4.1
-  HTTP_init();                                 //настраиваем HTTP интерфейс
-  SSDP_init();
 
-  tempTicker.attach(2, test);
-  // tempTicker.attach_ms(15000, collectTemperature);
-  // tempLimitTicker.attach_ms(10000, getTempLimit);
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    HTTP_init();                                 //настраиваем HTTP интерфейс
+    SSDP_init();
+  }
 }
 
 void loop()
 {
+  long currentMillis = millis();  
+
+  if (currentMillis - prevTempLimitMillis >= FETCH_TEMP_LIMITS_INTERVAL) {
+    prevTempLimitMillis = currentMillis;
+    getTempLimit();
+  }
+
+  if (currentMillis - prevSendTempMillis >= SEND_TEMP_INTERVAL) {
+    prevSendTempMillis = currentMillis;
+    collectTemperature();
+  }
+
   HTTP.handleClient();
   delay(1);
-}
-
-void test() {
-  WiFiClient httpClient;                                      //Обьявляем обьект класса WiFiClient
-  Serial.println("Test1");
-  Serial.println("Test2");
 }
 
 void HTTP_init(void)
@@ -80,12 +85,17 @@ void HTTP_init(void)
     HTTP.send(200, "text/plain", String(operatingMinimumEeprom) + "|" + String(operatingMaximumEeprom));
   });
   HTTP.on("/temp/set-limit", HTTP_GET, handleUpdateTempLimit);
+  HTTP.on("/reset", HTTP_GET, []() {
+    wifiManager.resetSettings();
+  });
+
   HTTP.begin();
 }
 
 void SSDP_init(void)
 {
-  SSDP.begin();
+  Serial.println("Starting SSDP...");
+  SSDP.setDeviceType("upnp:rootdevice");
   SSDP.setSchemaURL("description.xml");
   SSDP.setHTTPPort(80);
   SSDP.setName("ESP8266");
@@ -96,6 +106,7 @@ void SSDP_init(void)
   SSDP.setModelURL("http://esp8266-arduinoide.ru/wifimanager/");
   SSDP.setManufacturer("Alexandr Rudetski");
   SSDP.setManufacturerURL("http://www.esp8266-arduinoide.ru");
+  SSDP.begin();
 }
 
 void sendCurrentTemp()
@@ -114,31 +125,35 @@ void sendCurrentTemp()
     Serial.print(".");
     r++;
   }                                                            //Пока пытаемся соединиться с веб сервером отправляем в UART точки
-  if (r == 30) Serial.println(" Connection failed");           //Если не получилось соединиться пишем в UART, что не получилось
-  else Serial.println(" Connected to web");                    //Если получилось соединиться пишем в UART, что получилось
-  delay(1000);                                                 //Ждем
-  httpClient.connect(host, httpPort);
-  String Link;
-  Link = "/pokazaniya/temperature?" + params;                         //Формируем строку для GET запроса
-  httpClient.print(String("GET ") + Link + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"); //Отправляем GET запрос через ESP
-}
-
-void getTempLimit()
-{
-  WiFiClient httpClient;                                      //Обьявляем обьект класса WiFiClient
-  httpClient.setTimeout(1000);                                //Присваиваем значение паузы (1 секундa)
-  Serial.print("HTTP Connecting");                           //Пишем в UART: Соединяемся с нашим веб сервером
-  int r = 0;                                                  //Обьявляем переменную счетчика попыток подключения
-  while ((!httpClient.connect(host, httpPort)) && (r < 30)) {
-    delay(100);
-    Serial.print(".");
-    r++;
-  }                                                            //Пока пытаемся соединиться с веб сервером отправляем в UART точки
   if (r == 30) {
     Serial.println(" Connection failed");           //Если не получилось соединиться пишем в UART, что не получилось
   } else {
     Serial.println(" Connected to web");                    //Если получилось соединиться пишем в UART, что получилось
     delay(1000);                                                 //Ждем
+    httpClient.connect(host, httpPort);
+    String Link;
+    Link = "/pokazaniya/temperature?" + params;                         //Формируем строку для GET запроса
+    httpClient.print(String("GET ") + Link + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"); //Отправляем GET запрос через ESP
+  }
+}
+
+void getTempLimit() {
+  WiFiClient httpClient;                                      //Обьявляем обьект класса WiFiClient
+  httpClient.setTimeout(1000);                                //Присваиваем значение паузы (1 секундa)
+  Serial.print("HTTP Connecting");                           //Пишем в UART: Соединяемся с нашим веб сервером
+  int r = 0;                                                  //Обьявляем переменную счетчика попыток подключения
+
+  while ((!httpClient.connect(host, httpPort)) && (r < 30))
+  {
+    delay(100);
+    Serial.print(".");
+    r++;
+  }                                                            //Пока пытаемся соединиться с веб сервером отправляем в UART точки
+
+  if (r == 30) {
+    Serial.println(" Connection failed");           //Если не получилось соединиться пишем в UART, что не получилось
+  } else {
+    Serial.println(" Connected to web");                    //Если получилось соединиться пишем в UART, что получилось
     String Link2 = "/setting/setting?datchId=" + String(sensorId);                    //Формируем строку для GET запроса
     httpClient.print(String("GET ") + Link2 + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n"); //Отправляем GET запрос через ESP
     while (httpClient.connected()) {                             //Ловим ответ веб сервера
@@ -186,7 +201,6 @@ void getTempLimit()
       }
     }
   }
-  
 }
 
 void collectTemperature() {
